cb4df6b08929f4d6ffd4b33a4d9cde05418877be
Jayson Kesterson <jayson.kesterson24@gmail.com>
2020-04-22 21:45:44 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+function writeCards(array, name) {
+let newarray = [];
+for (let i = 0; i < array.length; i++) {
+    newarray.push(`Thank you, ${array[i]}, for the wonderful ${name} gift!`)
+  }
+  return newarray;
+}
+
+function countDown(number) {
+  let i = number;
+  while (i !== -1) {
+    console.log(i);
+    i--;
+  }
+}
