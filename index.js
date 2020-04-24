4bd3e29e3700ec828973febde940470636439e23
SantiagoSalazarPavajeau <santiago.s.pavajeau@gmail.com>
2020-04-24 12:07:25 +0200

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+let names = [];
+
+let event = "birthday";
+
+function writeCards( names , event) {
+    for (let i = 0; i !== names.length; i++) {
+    console.log(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  }
+  return names;
+
+}
+ 
+writeCards(names, event);
+
+function countDown(number){
+for (let countDown = number; countDown >= 0; countDown--) {
+    console.log(countDown);
+  }
+
+}
