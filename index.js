ea4d8dc3f549476529ded39d320aaeaf7bab084f
steelerx155 <steelerx155@gmail.com>
2020-05-01 21:18:48 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+function writeCards( namesArray, event ) {
+    let thankYouCards = []
+    for ( let i = 0; i < namesArray.length; i++ ) {
+      thankYouCards.push( `Thank you, ${namesArray[i]}, for the wonderful ${event} gift!` )
+    }
+    return thankYouCards
+  }
+  
+  function countDown( firstNumber ) {
+    while ( firstNumber > 0 ) {
+      console.log( firstNumber );
+      firstNumber -= 1;
+    }
+    console.log( firstNumber );
+  }
