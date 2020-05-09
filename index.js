adac550a2882029a52580d310849f9b47af20156
anikachristopher <anika.mchristoper@gmail.com>
2020-05-08 22:18:16 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solutions in this file
+
+
+const names = ["Lisa", "Kaitlin", "Jan"];
+
+function writeCards(names, message) {
+    let messages = []
+    for (let i = 0; i < names.length; i++) {
+        messages[i] = `Thank you, ${names[i]}, for the wonderful ${message} gift!`;
+    }
+    return messages;
+}
+
+// function countDown(integer){
+//     let countup = integer;
+//     while (countup >= integer) {
+//   console.log(countup--);
+//     }
+// }
+
+function countDown(number) {
+    let countup = number;
+    while (countup >= 0) {
+  console.log(countup--);
+    }
+}
